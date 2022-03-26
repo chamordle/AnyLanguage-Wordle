aa31d31ea2357d7424ec7d34e1b8c39995673c8e
chamordle <102394095+chamordle@users.noreply.github.com>
2022-03-26 12:37:50 +1000

    Update words.ts

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -3,8 +3,14 @@ import { VALIDGUESSES } from '../constants/validGuesses'
 import { CONFIG } from '../constants/config'
 
 export const isWordInWordList = (word: string) => {
-  return WORDS.includes(word) || VALIDGUESSES.includes(word)
+  return WORDS.includes(word) || VALIDGUESSES.includes(deaccent(word))
 }
+
+export const deaccent = (word: string) => {
+	word = word.normalize("NFD").replace(/[\u0300-\u036f]/g, "");  //NFD : Canonical Unicode Decomposition
+	return word;
+}
+
 
 export const isWinningWord = (word: string) => {
   return solution === word
